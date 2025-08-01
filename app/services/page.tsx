5e7065de2437c22c4fd025e88c04e40b622bf2f5
aqlhsyfqhsym <nuraqilah.syafiqah@yahoo.com>
2025-08-01 16:31:44 +0800

    fix padding

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,93 +5,72 @@ import CardRefinery from "@/components/organisms/CardRefinery";
 import CardGetInTouch from "@/components/organisms/CardGetInTouch";
 import CardContact from "@/components/organisms/CardContact";
 import CardSuggest from "@/components/organisms/CardSuggest";
- import services from "@/data/services.json";
+import services from "@/data/services.json";
 import MainTitle from "@/components/atoms/Heading";
 import CardStep from "@/components/organisms/CardStep";
 import Image from "next/image";
 import Slicker from "@/components/organisms/SlickAbout";
+import SectionIntro from "@/components/molecules/SectionIntro";
 
 export default function About() {
-
-    const factoryImages = [
-         '/images/slick/factory 1.png',
-         '/images/slick/factory 2.png',
-         '/images/slick/factory 3.png',
-         '/images/slick/factory 4.png',
-         '/images/slick/factory 1.png', 
-         '/images/slick/factory 2.png'
-    ];
+  const factoryImages = [
+    "/images/slick/factory 1.png",
+    "/images/slick/factory 2.png",
+    "/images/slick/factory 3.png",
+    "/images/slick/factory 4.png",
+    "/images/slick/factory 1.png",
+    "/images/slick/factory 2.png",
+  ];
 
   return (
     <div className="max-w-none font-[family-name:var(--font-geist-sans)] p-5 min-h-screen xl:px-32 xl:py-18 mx-auto ">
       <VideoBanner videoSrc="/video/biogreen_video.mp4" />
 
-      <div className="xl:px-64 lg:px-40 md:px-20 sm:px-10 mt-12">
-        <div className="text-center mb-8">
-          <MainTitle heading={services.coreValues.heading} />
-        </div>
+      <section>
+        <MainTitle heading={services.coreValues.heading} />
         <p className="xl:px-60 lg:px-40 md:px-20 sm:px-10 text-green-primary text-2xl text-center font-medium leading-relaxed">
           {services.tmdDescription}
         </p>
-        {/* Why Us Cards */}
         <CardWhyUs cards={services.whyUs} />
+      </section>
 
-        <div className="text-center mb-8 mt-20">
-          <MainTitle heading={services.coreValues.heading2} />
-        </div>
-        
+      <section>
+        <MainTitle heading={services.coreValues.heading2} />
         <p className="xl:px-52 lg:px-40 md:px-20 sm:px-10 text-green-primary text-2xl text-center font-medium leading-relaxed">
           {services.tmdDescription2}
         </p>
-      
-        {/* Refinery Process Cards */}
         <CardRefinery cards={services.refineryProcess} />
-
-        {/* Steps Cards */}
         <CardStep steps={services.steps} />
+      </section>
 
-        <div className="text-center mb-8 mt-36">
-          <MainTitle heading={services.coreValues.heading3} />
-        </div>
+      <section>
+        <MainTitle heading={services.coreValues.heading3} />
         <p className="xl:px-52 lg:px-40 md:px-20 sm:px-10 text-green-primary text-2xl text-center font-medium leading-relaxed">
           {services.tmdDescription3}
         </p>
-
-        {/* Company Partners Carousel */}
         <SlickCompany />
-          
-        <div className="text-center mb-8 mt-36">
-          <MainTitle heading={services.coreValues.heading4} />
-        </div>
+      </section>
+
+      <section>
+        <MainTitle heading={services.coreValues.heading4} />
         <p className="xl:px-52 lg:px-40 md:px-20 sm:px-10 text-green-primary text-2xl text-center font-medium leading-relaxed">
           {services.tmdDescription4}
         </p>
-
-        {/* Get In Touch Cards */}
         <CardGetInTouch />
-
         <p className="xl:px-52 lg:px-40 md:px-20 sm:px-10 text-green-primary text-2xl text-center font-medium leading-relaxed">
-          {services.tmdDescription5}
-        </p>
-
-        {/* Staff Contact Cards */}
+          {services.tmdDescription5} 
+        </p> 
         <CardContact />
-
-        {/* Suggestion Form */}
         <CardSuggest />
+      </section>
 
-        <div className="flex items-left gap-2 xl:pr-52 pl-0 lg:pr-40 pl-0 md:pr-20 pl-0 sm:pr-10 pl-0 pt-20 pb-2">
-          <Image src="/icons/square.svg" alt="square" width={14} height={14} />
-          <p className="text-green-secondary font-semibold uppercase text-xs tracking-widest font-kumbh">
-            Latest Gallery
-          </p>
-        </div>
-
-        <p className="xl:pr-52 pl-0 lg:pr-40 pl-0 md:pr-20 pl-0 sm:pr-10 pl-0 text-green-primary text-3xl font-medium leading-relaxed">
-          {services.tmdDescription6}
-        </p>
-      </div>
-
+      <section>
+        <SectionIntro
+          iconSrc="/icons/square.svg"
+          label="Latest Gallery"
+          heading={<>Photos of our oil recycling factories</>}
+        />
+      </section>
       {/* Factory Gallery Carousel */}
       <Slicker images={factoryImages} />
     </div>
